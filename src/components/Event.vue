1c05955abf1e5ef0ce3e14b666bca0674bc521e1
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-05 08:26:53 -0800

    Biggest change. Added css and font folders and changed default font to open sans.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,13 +1,39 @@
 <template lang="html">
-  <div style="backgroundColor: gray; padding: 0 10px 0 10px; margin: 0 10px 0 10px">
-    Dominic Chavez snipped a coupon.
+  <div class="event-container">
+    <div class="pull-left">
+        <!-- <img src="{{photoUrl}}"> -->
+        {{ username }} {{ action }} ({{ location }})
+    </div>
+    <div class="pull-right">
+      {{ time }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      photoUrl: "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
+      username: "Dominic Chavez",
+      action: "snipped a coupon",
+      location: "Philadelphia, PA",
+      time: "30 min ago",
+    }
+  },
+
 }
 </script>
 
-<style lang="css">
+<style scoped>
+  .img {
+    height: 40px;
+    width: 40px;
+  }
+  .event-container {
+    height: 60px;
+    background-color: gray;
+    padding: 0 10px 0 10px;
+    margin: 0 10px 0 10px;
+  }
 </style>
