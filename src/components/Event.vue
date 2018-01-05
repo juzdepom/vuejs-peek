7fd5eb9d2f95e0ca2a1d3bc2eefc38419c962d0a
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-05 09:16:42 -0800

    event improvements. see Screenshot.pn

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,11 +1,12 @@
 <template lang="html">
   <div class="event-container">
     <div class="pull-left">
-        <!-- <img src="{{photoUrl}}"> -->
-        {{ username }} {{ action }} ({{ location }})
+
+        <p><img class="profile-photo" :src="photoUrl" alt=""><strong>{{ username }} {{ action }}</strong> ({{ location }})</p>
     </div>
     <div class="pull-right">
-      {{ time }}
+      <p>{{ time }} <img class="three-dots" src="../assets/three_dots@2x.png"></p>
+
     </div>
   </div>
 </template>
@@ -14,7 +15,7 @@
 export default {
   data(){
     return {
-      photoUrl: "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
+      photoUrl: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png',
       username: "Dominic Chavez",
       action: "snipped a coupon",
       location: "Philadelphia, PA",
@@ -26,13 +27,42 @@ export default {
 </script>
 
 <style scoped>
+  div {
+    font-family: Open Sans;
+  }
+  .three-dots{
+    height: 16px;
+    margin-left: 15px;
+    margin-right: 20px;
+
+  }
+  .profile-photo {
+    height: 40px;
+    border-radius: 25px;
+    margin-right: 10px;
+  }
+  strong {
+    font-weight: 600;
+  }
+  p {
+    color: #5C5C5C;
+  }
+  .pull-left p {
+    font-size: 14px;
+  }
+  .pull-right p {
+    font-size: 12px;
+  }
   .img {
     height: 40px;
     width: 40px;
   }
   .event-container {
+    line-height: 60px;
     height: 60px;
-    background-color: gray;
+    cursor: pointer;
+    border-radius: 10px;
+    background-color: white;
     padding: 0 10px 0 10px;
     margin: 0 10px 0 10px;
   }
--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6">
-    <div class="panel panel-success">
+    <!-- <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
           {{ stock.name }}
@@ -18,7 +18,6 @@
             >
         </div>
         <div class="pull-right">
-          <!-- quantity % 1 makes sure quantity isn't decimal -->
           <button
             class="btn btn-success"
             @click="buyStock"
@@ -27,51 +26,43 @@
             {{ insufficientFunds ? 'Insufficient Funds' : 'Buy'}}
           </button>
         </div>
-
-
       </div>
-    </div>
+    </div> -->
   </div>
 
 </template>
 
-<style scoped>
-  .danger {
-    border: 1px solid red;
-  }
-</style>
-
 <script>
 export default {
-  props: ['stock'],
-  data(){
-    return {
-      quantity: 0,
-    }
-  },
-  computed:{
-    insufficientFunds(){
-      return (this.quantity * this.stock.price) > this.$store.getters.funds;
-    },
-    totalPrice(){
-      if(this.quantity > 0){
-        return '| Total Price: ' + (this.quantity * this.stock.price)
-      } else {
-        return ''
-      }
-    }
-  },
-  methods: {
-    buyStock(){
-      const order = {
-        stockId: this.stock.id,
-        stockPrice: this.stock.price,
-        quantity: this.quantity,
-      };
-      this.$store.dispatch('buyStock', order);
-      console.log(order)
-      this.quantity = 0;
-    }
-  }
-}
+//   props: ['stock'],
+//   data(){
+//     return {
+//       quantity: 0,
+//     }
+//   },
+//   computed:{
+//     insufficientFunds(){
+//       return (this.quantity * this.stock.price) > this.$store.getters.funds;
+//     },
+//     totalPrice(){
+//       if(this.quantity > 0){
+//         return '| Total Price: ' + (this.quantity * this.stock.price)
+//       } else {
+//         return ''
+//       }
+//     }
+//   },
+//   methods: {
+//     buyStock(){
+//       const order = {
+//         stockId: this.stock.id,
+//         stockPrice: this.stock.price,
+//         quantity: this.quantity,
+//       };
+//       this.$store.dispatch('buyStock', order);
+//       console.log(order)
+//       this.quantity = 0;
+//     }
+//   }
+// }
 </script>
